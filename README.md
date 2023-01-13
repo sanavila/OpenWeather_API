26f076946dcae627d83247f97284786acb7b6d2b
sanavila <rute.leone@hotmail.com>
2023-01-13 17:59:03 -0300

    consumindo api

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# OpenWeather_API
+### Sobre
+O projeto tem a utilidade de trazer informações sobre o tempo atual das cidades do mundo inteiro através da utilização da OpenWeather_API.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
 // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=3aa4b242a97516bb52c940222be7d024
+// Variáveis e seleção de eventos
+const apiKey = "3aa4b242a97516bb52c940222be7d024";
+const countryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const weatherContainer =document.querySelector("#weather-data");
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+// Funções
+const getWeatherData = async (city) => {
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+  const res = await fetch(apiWeatherURL);
+  const data = await res.json();
+  return data;
+};
+const showWeatherData = async (city) => {
+  const data = await getWeatherData(city);
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+  countryElement.setAttribute(
+    "src",
+    countryURL + data.sys.country);
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}%km/h`;
+  weatherContainer.classList.remove("hide");
+};
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = cityInput.value;
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if(e.code === "Enter") {
+    const city = e.target.value;
+    showWeatherData(city);
+  }
+});
